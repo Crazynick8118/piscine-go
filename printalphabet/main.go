2f04b571ce5ea20009a48ad79b128dea0a30088a
Crazynick8118 <abilgazin.nurzhan@gmail.com>
2019-10-16 22:42:21 +0600

    q2e1

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,10 +3,10 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-		 i := 97 
-		 for i <= 122 {
-			z01.PrintRune(rune(i))	
-			i++		
-		}
-		z01.PrintRune('\n')
+	i := 97
+	for i <= 122 {
+		z01.PrintRune(rune(i))
+		i++
+	}
+	z01.PrintRune('\n')
 }
