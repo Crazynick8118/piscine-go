b896caa192759118e5f302041e99825e4c5219cf
Crazynick8118 <abilgazin.nurzhan@gmail.com>
2019-10-16 22:22:04 +0600

    Maingo

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,9 +2,9 @@ package main
 import "github.com/01-edu/z01"
 func main() {
 		 i :=97 
-		 for i <= 122 ; {
+		 for i <= 122 {
 			z01.PrintRune(rune(i))	
 			i++		
 		}
-		z01.PrintRune(10)
+		z01.PrintRune('\n')
 }
