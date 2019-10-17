dfed40f67a573f0495ea46528063d6de1e44857e
Crazynick8118 <abilgazin.nurzhan@gmail.com>
2019-10-17 23:41:21 +0600

    combo

--- test/PrintComb/PrintComb.go
+++ test/PrintComb/PrintComb.go
@@ -5,15 +5,12 @@ import "github.com/01-edu/z01"
 func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		for k := i + 1; k <= '9'; k++ {
-			for l := k + 1; l <= '9'; l++ {
+			for j := k + 1; j <= '9'; j++ {
 				z01.PrintRune(i)
 				z01.PrintRune(k)
-				z01.PrintRune(l)
-				if i != '7' || k != '8' || l != '9' {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-				}
+				z01.PrintRune(j)
 			}
+
 		}
 	}
 	z01.PrintRune('\n')
